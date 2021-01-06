37a5d943c0c5e29a6a1cff252b1d395c5224013d
Tomasz Kozysa <tomek@15nine.com>
2021-01-06 09:40:46 +0000

    copy tweak

--- pages/case-studies/the-brit-awards.vue
+++ pages/case-studies/the-brit-awards.vue
@@ -45,7 +45,7 @@ export default {
                                             
                         <p class="cs-single-copy">The Brit Awards is one of the biggest live international awards events across the music industry, we were commissioned to work with the show producers BPI and broadcaster ITV to introduce a global first live social vote on twitter for Best British Video.</p>
                         <p class="cs-single-copy">Whist this was initiated from previously running a successful social vote on ‘The Mobo Awards’ on ITV earlier in the year, this was a huge challenge in terms of bringing a new voting mechanic to a show that historically had never had a public vote element for one of their awards categories previously. </p>
-                        <p class="cs-single-copy">This vote needed to deal with unprecedented numbers of concurrent engagements on twitter making sure that votes were capped to one social vote from each social profile for each nominated artist as well as to provide a bulletproof audit trail, so the social voting could stand up against any similar telephony vote. The social vote helped to contribute to the massive 7.8 total international tweets that the show received on the night. </p>
+                        <p class="cs-single-copy">This vote needed to deal with unprecedented numbers of concurrent engagements on twitter making sure that votes were capped to one social vote from each social profile for each nominated artist as well as to provide a bulletproof audit trail, so the social voting could stand up against any similar telephony vote. The social vote helped to contribute to the massive 7.8 million total international tweets that the show received on the night. </p>
 
 
 
