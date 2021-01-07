d5ffff4a1ae5b1a2b490500f0571d678f3868c86
Tomasz Kozysa <tomek@15nine.com>
2021-01-07 16:38:19 +0000

    favicon and social

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,10 +8,16 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Broadcast & live event innovation consultancy specialising in audience interaction & fan engagement with content & technology' }
+      { hid: 'description', name: 'description', content: 'Broadcast & live event innovation consultancy specialising in audience interaction & fan engagement with content & technology' },
+      { hid: 'og:image', property: 'og:image', content: "/og-image.jpg" },
+      { hid: 'og:image:height', property: 'og:image:height', content: "126" },
+      { hid: 'og:description', property: 'og:description', content: "Broadcast &amp; live event innovation consultancy specialising in audience interaction &amp; fan engagement with content &amp; technology" },
+      { hid: 'og:title', property: 'og:title', content: "Hypothesis Media" },
+      { hid: 'og:title', property: 'og:url', content: "hypothesis-media.co.uk" },
+      
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
 
